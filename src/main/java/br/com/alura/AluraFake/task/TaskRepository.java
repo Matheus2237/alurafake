13715a093e060b6481e82cab8e9f2f50ff59ab49
Matheus2237 <mathpaulinor@gmail.com>
2025-04-29 22:24:37 -0300

    chore: Removoval of unused method and it's tests

--- src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/task/TaskRepository.java
@@ -3,10 +3,7 @@ package br.com.alura.AluraFake.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    Optional<Task> findByCourseIdAndStatement(Long courseId, String duplicatedStatement);
 }
